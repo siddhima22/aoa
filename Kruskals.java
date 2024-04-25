fee755afd2f86e092dc19195756791818b8343bd
siddhima22 <114576057+siddhima22@users.noreply.github.com>
2024-04-25 16:03:39 +0530

    Add files via upload

---
+++ Kruskals.java
@@ -0,0 +1,127 @@
+import java.util.ArrayList; 
+import java.util.Comparator; 
+import java.util.List; 
+import java.util.Scanner;
+
+public class Kruskals { 
+  
+    static class Edge { 
+        int src, dest, weight; 
+  
+        public Edge(int src, int dest, int weight) 
+        { 
+            this.src = src; 
+            this.dest = dest; 
+            this.weight = weight; 
+        } 
+    } 
+  
+    static class Subset { 
+        int parent, rank; 
+  
+        public Subset(int parent, int rank) 
+        { 
+            this.parent = parent; 
+            this.rank = rank; 
+        } 
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of vertices: ");
+        int V = scanner.nextInt(); 
+        
+        System.out.print("Enter the number of edges: ");
+        int E = scanner.nextInt(); 
+
+        List<Edge> graph = new ArrayList<Edge>();
+
+        System.out.println("Enter the source, destination, and weight of each edge:");
+        for (int i = 0; i < E; i++) {
+            System.out.print("Edge " + (i + 1) + ": ");
+            int src = scanner.nextInt();
+            int dest = scanner.nextInt();
+            int weight = scanner.nextInt();
+            graph.add(new Edge(src, dest, weight));
+        }
+
+        graph.sort(new Comparator<Edge>() { 
+            @Override public int compare(Edge o1, Edge o2) 
+            { 
+                return o1.weight - o2.weight; 
+            } 
+        }); 
+  
+        kruskals(V, graph); 
+        scanner.close();
+    } 
+  
+    private static void kruskals(int V, List<Edge> edges) 
+    { 
+        int j = 0; 
+        int noOfEdges = 0; 
+  
+        Subset subsets[] = new Subset[V]; 
+        Edge results[] = new Edge[V]; 
+  
+        for (int i = 0; i < V; i++) { 
+            subsets[i] = new Subset(i, 0); 
+        } 
+  
+        while (noOfEdges < V - 1) { 
+            Edge nextEdge = edges.get(j); 
+            int x = findRoot(subsets, nextEdge.src); 
+            int y = findRoot(subsets, nextEdge.dest); 
+  
+            if (x != y) { 
+                results[noOfEdges] = nextEdge; 
+                union(subsets, x, y); 
+                noOfEdges++; 
+            } 
+  
+            j++; 
+        } 
+  
+        System.out.println( 
+            "Following are the edges of the constructed MST:"); 
+        int minCost = 0; 
+        for (int i = 0; i < noOfEdges; i++) { 
+            System.out.println(results[i].src + " -- "
+                               + results[i].dest + " == "
+                               + results[i].weight); 
+            minCost += results[i].weight; 
+        } 
+        System.out.println("Total cost of MST: " + minCost); 
+    } 
+  
+    private static void union(Subset[] subsets, int x, 
+                              int y) 
+    { 
+        int rootX = findRoot(subsets, x); 
+        int rootY = findRoot(subsets, y); 
+  
+        if (subsets[rootY].rank < subsets[rootX].rank) { 
+            subsets[rootY].parent = rootX; 
+        } 
+        else if (subsets[rootX].rank 
+                 < subsets[rootY].rank) { 
+            subsets[rootX].parent = rootY; 
+        } 
+        else { 
+            subsets[rootY].parent = rootX; 
+            subsets[rootX].rank++; 
+        } 
+    } 
+  
+    private static int findRoot(Subset[] subsets, int i) 
+    { 
+        if (subsets[i].parent == i) 
+            return subsets[i].parent; 
+  
+        subsets[i].parent 
+            = findRoot(subsets, subsets[i].parent); 
+        return subsets[i].parent; 
+    } 
+}
